5f9f3d524e5542ea4e214eedec966609a01f3c77
Matthias Müller <matthias_001@web.de>
2017-09-29 12:57:44 +0200

    Design and template editing

--- inc/BestsellerWidget.php
+++ inc/BestsellerWidget.php
@@ -44,6 +44,7 @@ class BestsellerWidget extends WP_Widget {
 					<div class="text-center">
 						<a class="neat-link" href="<?= $bestseller->AmazonUrl; ?>" title="<?= $bestseller->Longname; ?> auf Amazon ansehen" target="_blank" rel="nofollow"><?= $bestseller->RatingStars; ?></a>
 					</div>
+					<a class="neat-link" href="<?= $bestseller->Permalink;?>" title="<?= $bestseller->Longname;?> Produktbeschreibung lesen"><h3><?= $bestseller->Longname;?></h3></a>
 					<div class="text-center product--price">
 						<a class="neat-link" href="<?= $bestseller->AmazonUrl; ?>" title="<?= $bestseller->Longname; ?> auf Amazon ansehen" target="_blank" rel="nofollow"><?= $bestseller->AmazonPriceFormatted; ?></a>
 					</div>
